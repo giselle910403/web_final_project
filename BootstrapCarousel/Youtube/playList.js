8b9d5d392bf8cf2f94fd0e08cacae787df727b94
gislle910403 <giselle910403@gmail.com>
2021-06-25 20:55:07 +0800

    First commit

---
+++ BootstrapCarousel/Youtube/playList.js
@@ -0,0 +1,12 @@
+//YouTube Video ID
+let playList = [
+    "SPxTgcLdyP4",
+    "g3gS0Yrd-yk",
+    "N6fsTzs4lgQ"
+];
+//播放起訖秒數
+let playTime = [
+    [0, 5],
+    [0, 5],
+    [0, 6]
+];
---
+++ BootstrapCarousel/people/main.js
@@ -0,0 +1,31 @@
+/*let person={
+    firstName:"Apple",
+    lastName:"Chien",
+    height:153
+};
+
+for(x in person){
+    //console.log(x+":"+person[x]);
+    console.log(`${x}:${person[x]}`)
+}
+
+for(x in window){
+    console.log(x+":"+window[x]);
+}*/
+let thisH1=document.getElementsByTagName("h1")[0];
+let thisP=document.getElementsByTagName("p")[0];
+let thisDiv=document.getElementsByTagName("div")[0];
+
+function mouseIn(){
+    thisH1.innerHTML="你進來了";
+}
+function mouseOut(){
+    thisH1.innerHTML="你出去了";
+    thisP.innerHTML="";
+}
+function mouseMove(e){
+    thisP.innerHTML="你在裡面走來走去，位置: "+e.clientX+","+e.clientY;
+}
+thisDiv.addEventListener("mouseover",mouseIn);
+thisDiv.addEventListener("mouseout",mouseOut);
+thisDiv.addEventListener("mousemove",mouseMove);
